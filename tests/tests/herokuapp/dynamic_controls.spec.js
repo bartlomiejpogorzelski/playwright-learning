110bd7206c531ff88d9a82eae42cba18425f443a
Bartlomiej <bartlomiej.pogorzelski@onet.pl>
2025-12-13 17:52:32 +0100

    Dynamic_controls: enabled/disabled button

--- tests/tests/herokuapp/dynamic_controls.spec.js
+++ tests/tests/herokuapp/dynamic_controls.spec.js
@@ -22,5 +22,29 @@ test("Dynaminc control - removing and adding checkboxes", async({page}) => {
   //await page.waitForSelector('#checkbox', { state: 'attached', timeout: 10000 });
   await expect(checkbox).toBeVisible();
 
+
+  const inputText = page.locator("#input-example input");
+  const enableButton = page.locator("#input-example button");
+  const messageVisibility = page.locator('#input-example #message');
+
+  await expect(inputText).toBeDisabled();
+  await expect(messageVisibility).toBeHidden();
+  
+  await enableButton.click();
+  await expect(inputText).toBeEnabled();
+  await inputText.fill("Some text: asdf")
+  await expect(messageVisibility).toBeVisible();
+  await expect(messageVisibility).toHaveText("It's enabled!");
+  
+  await enableButton.click();
+  await expect(inputText).toBeDisabled();
+  await expect(inputText).toHaveValue("Some text: asdf");
+  await expect(messageVisibility).toBeVisible();
+  await expect(messageVisibility).toHaveText("It's disabled!");
+
+
+
+
+
 }
 );
