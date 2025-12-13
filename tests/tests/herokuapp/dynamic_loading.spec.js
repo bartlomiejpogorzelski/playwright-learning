7370624e7916124a7fabf0d2abc045ebe459c468
Bartlomiej <bartlomiej.pogorzelski@onet.pl>
2025-12-13 21:52:07 +0100

    Dynamic_loading hidden element

---
+++ tests/tests/herokuapp/dynamic_loading.spec.js
@@ -0,0 +1,21 @@
+// @ts-check
+const { test, expect } = require('@playwright/test');
+const { TIMEOUT } = require('node:dns');
+
+test("Dynaminc loading - element appears after clicking Start ", async({page}) => {
+  await page.goto('https://the-internet.herokuapp.com/dynamic_loading/1');
+
+  const button = page.locator('#start button');
+  const loading = page.locator('#loading');
+  const text = page.locator('#finish h4');
+
+  await expect(text).toBeHidden();
+  await expect(loading).toBeHidden();
+
+  await button.click();
+  await expect(loading).toBeVisible();
+  await expect(text).toBeVisible({ timeout: 10000 });
+  await expect(text).toHaveText('Hello World!');
+
+});
+    
