36e2adff586b3ed528c8e94a2d032a858aab149a
Bartlomiej <bartlomiej.pogorzelski@onet.pl>
2025-12-15 21:25:41 +0100

    Horizontal Slider

---
+++ tests/tests/herokuapp/horizontal_slider.spec.js
@@ -0,0 +1,22 @@
+// @ts-check
+const { test, expect } = require('@playwright/test');
+
+test('Horizontal slider - set values', async({ page }) => {
+  await page.goto('https://the-internet.herokuapp.com/horizontal_slider');
+
+  const slider = page.locator('input[type="range"]');
+  const value = page.locator('#range');
+
+  await expect(slider).toBeVisible();
+  await expect(slider).toHaveValue('0');
+
+  await expect(value).toHaveText('0');
+
+  await slider.focus();
+  // step = 0.5
+  for(let i = 0; i<9; i++) {
+    await page.keyboard.press('ArrowRight');
+  }
+
+  await expect(value).toHaveText('4.5');
+});
