8890b9b5d37a45c48a08c5415daf05de8848c850
Bartlomiej <bartlomiej.pogorzelski@onet.pl>
2025-12-15 19:29:46 +0100

    Test for geolocation, permission

---
+++ tests/tests/herokuapp/geolocation.spec.js
@@ -0,0 +1,25 @@
+// @ts-check
+const { test, expect } = require('@playwright/test');
+
+test("Geolocation - permission with latitude and longitude ", async ({page, context}) => {
+
+  await context.grantPermissions(['geolocation']);
+  // mock:
+  await context.setGeolocation({
+    latitude: 52.2297,
+    longitude: 21.0122,
+  });
+
+  await page.goto('https://the-internet.herokuapp.com/geolocation');
+
+  const button = page.locator('#content button');
+  const latitude = page.locator('#lat-value');
+  const longitude = page.locator('#long-value');
+
+  await button.click();
+  await expect(latitude).toHaveText("52.2297");
+  await expect(longitude).toHaveText("21.0122");
+
+
+
+});
