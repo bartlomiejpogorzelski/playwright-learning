0d22bb812e31302598d68de1b7ad5f9e68d5092f
Bartlomiej <bartlomiej.pogorzelski@onet.pl>
2025-12-21 18:54:21 +0100

    Inputs

---
+++ tests/tests/herokuapp/inputs.spec.js
@@ -0,0 +1,24 @@
+// @ts-check
+const { test, expect } = require('@playwright/test');
+
+test('Inputs - numbers field', async({ page }) => {
+  await page.goto('https://the-internet.herokuapp.com/inputs');
+
+  
+  const input = page.locator('input[type="number"]');
+  await expect(input).toBeVisible();
+
+  await input.fill("123123");
+  await expect(input).toHaveValue("123123");
+
+  await input.press("ArrowDown");
+  await expect(input).toHaveValue("123122");
+
+  await input.press("ArrowUp");
+  await expect(input).toHaveValue("123123");
+
+  await input.clear();
+  await expect(input).toHaveValue('');
+
+
+});
